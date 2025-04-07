78c9d5d0c5b0568b9675c313d91e6b21bb074b29
cyLee1111 <flora021111@gmail.com>
2025-04-07 16:57:31 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main-container">
+    <div class="hello">
+      <p>hello vue!!</p>
+      <p>hi hi</p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.hello {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import App from '@/App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'app',
+      component: App,
+    },
+  ],
+});
+
+export default router;
