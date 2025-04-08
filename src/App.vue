41b292c33ff0ee4d7d1d2a9cbc0574ef3922e556
yeomjungwoo <yeomi0206@gmail.com>
2025-04-08 16:30:17 +0900

    feat: Add temporary navigation bar(App.vue)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="main-container">
     <div class="hello"></div>
+    <!-- 네비게이션 바를 항상 두되, 페이지별로 조건을 추가하여 보이거나 숨기기 -->
+    <!-- 첫 페이지에서 네비게이션 바 보이도록, 그 외 페이지에서는 안 보이게 처리 -->
     <nav class="navbar">
       <router-link to="/pokedex">📘 도감</router-link>
       <router-link to="/money-tracker">💰 가계부</router-link>
