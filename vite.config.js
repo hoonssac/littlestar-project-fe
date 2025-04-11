b3317b49127322a3e4b5c9b03a9f7a9266cb2aab
SangHoon <112858332+Hoonssac@users.noreply.github.com>
2025-04-11 09:45:04 +0900

    feat: ready for deploy

--- vite.config.js
+++ vite.config.js
@@ -15,11 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-<<<<<<< HEAD
         target: 'https://laced-thread-brother.glitch.me',
-=======
-        target: 'http://localhost:3000',
->>>>>>> 5253a2919eb3753610e1321b467d20188eb2fa06
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
